146bdf70af3da6adb1e46fa8f3a3f5e5ffcee5ae
Maciej Sobolewski <maciej.sobolewski@stxnext.pl>
2021-06-29 11:27:02 +0200

    Change app name.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-ngx-treeview app is running!');
+    expect(page.getTitleText()).toEqual('ngx-treeview app is running!');
   });
 
   afterEach(async () => {
--- gh-pages.js
+++ gh-pages.js
@@ -1,6 +1,6 @@
 const ghpages = require('gh-pages');
 
-ghpages.publish('dist/angular-ngx-treeview', {
+ghpages.publish('dist/ngx-treeview', {
   repo: 'https://' + process.env.GH_TOKEN + '@github.com/' + process.env.TRAVIS_REPO_SLUG + '.git'
 }, function (err) {
   if (err) {
