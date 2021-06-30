d3fbe8a36b1588545b80cab86d1ba81da6c5c22d
Maciej Sobolewski <maciej.sobolewski@stxnext.pl>
2021-06-30 12:06:12 +0200

    Proper path in install scripts.

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ if pwd | grep node_modules ; then
   cd tmp-ngx-treeview
   npm install --ignore-scripts
   npm rebuild node-sass
-  ng build
+  npm run build
   cd ..
-  mv tmp-ngx-treeview/dist/src node_modules/ngx-treeview
+  mv tmp-ngx-treeview/src/lib node_modules/ngx-treeview
   mv tmp-ngx-treeview node_modules/ngx-treeview/source
 fi
