a945e03cef9750dbcc503822b03e464e3d2f746c
Maciej Sobolewski <maciej.sobolewski@stxnext.pl>
2021-06-30 10:43:39 +0200

    Modify install script path.

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ if pwd | grep node_modules ; then
   npm rebuild node-sass
   npm run build
   cd ..
-  mv tmp-ngx-treeview/dist/src node_modules/ngx-treeview
+  mv tmp-ngx-treeview/src node_modules/ngx-treeview
   mv tmp-ngx-treeview node_modules/ngx-treeview/source
 fi
